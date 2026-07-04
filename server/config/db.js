9fe4aa2436b014d6f03cd087e5f86cd98c58ead6
ChanhLiz <chanhliz152635@gmail.com>
2026-07-04 14:31:00 +0700

    db js

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "123456", // Đổi mật khẩu theo cài đặt của bạn
-  database: "ecommerce_db",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
