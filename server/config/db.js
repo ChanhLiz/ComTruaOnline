bf3f9f05552fca93cb9680ef2293db679f08ab16
ChanhLiz <chanhliz152635@gmail.com>
2026-07-04 16:13:26 +0700

    222

--- server/config/db.js
+++ server/config/db.js
@@ -1,30 +1,12 @@
 require("dotenv").config();
 const mysql = require("mysql2/promise");
 
-const isRailway =
-  process.env.RAILWAY_ENVIRONMENT ||
-  process.env.RAILWAY_PROJECT_ID;
-
 const pool = mysql.createPool({
-  host: isRailway
-    ? process.env.DB_HOST
-    : (process.env.LOCAL_DB_HOST || "localhost"),
-
-  port: isRailway
-    ? Number(process.env.DB_PORT)
-    : Number(process.env.LOCAL_DB_PORT || 3306),
-
-  user: isRailway
-    ? process.env.DB_USER
-    : (process.env.LOCAL_DB_USER || "root"),
-
-  password: isRailway
-    ? process.env.DB_PASSWORD
-    : (process.env.LOCAL_DB_PASSWORD || ""),
-
-  database: isRailway
-    ? process.env.DB_NAME
-    : (process.env.LOCAL_DB_NAME || "ecommerce_db"),
+  host: process.env.DB_HOST || process.env.LOCAL_DB_HOST,
+  port: Number(process.env.DB_PORT || process.env.LOCAL_DB_PORT),
+  user: process.env.DB_USER || process.env.LOCAL_DB_USER,
+  password: process.env.DB_PASSWORD || process.env.LOCAL_DB_PASSWORD,
+  database: process.env.DB_NAME || process.env.LOCAL_DB_NAME,
 
   waitForConnections: true,
   connectionLimit: 10,
