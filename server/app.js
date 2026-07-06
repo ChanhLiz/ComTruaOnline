658259e8131c97008fabe4040c411f459fbc70e3
ChanhLiz <chanhliz152635@gmail.com>
2026-07-06 10:46:34 +0700

    finallllllll

--- server/app.js
+++ server/app.js
@@ -12,8 +12,8 @@ const weeklyMenuRoutes = require("./routes/weeklyMenuRoutes");
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 app.use(express.static(path.join(__dirname, "../client")));
 
